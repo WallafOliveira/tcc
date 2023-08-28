63c12c76b765b8bcaed949693b34c2cf0b9ea257
Wallaf Oliveira <106490011+WallafOliveira@users.noreply.github.com>
2023-08-28 10:53:26 -0300

    update wallaf

--- src/questao_pai/questao_pai.jsx
+++ src/questao_pai/questao_pai.jsx
@@ -11,13 +11,35 @@ function QuestaoPai() {
 
     return (
 
-
+            <div>
             <div>
 
                 <nav className="cabeçalho">
                 <img className="logo" src={logo} alt='Eduquiz' onClick={ () => { navigate('/') }}/> 
                 </nav>
+            </div>
 
+            <div class="radio-group">
+
+        <label class="radio-mat">
+            <input type="radio" name="opcao" value="Matematica"/>  Matematica  </label>
+
+        <label class="radio-pot">
+            <input type="radio" name="opcao" value="Portugues"/> Portugues </label>
+
+        <label class="radio-geo">
+            <input type="radio" name="opcao" value="Geografia"/>  Geografia </label>
+
+        <label class="radio-his">
+            <input type="radio" name="opcao" value="Hístoria"/> Hístoria  </label>
+
+        <label class="radio-cie">
+            <input type="radio" name="opcao" value="Ciência"/> Ciência  </label>
+
+        <label class="radio-ing">
+            <input type="radio" name="opcao" value="Inglês  "/> Inglês  </label>
+
+    </div>
 
             </div>
 
