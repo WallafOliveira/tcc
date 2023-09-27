9feb9a120af6a565dee7bd060973f332af572b4b
VictorHerrero16 <herrerovictor05@gmail.com>
2023-09-27 13:40:10 -0300

    Update questoes.jsx

--- src/map/questoes.jsx
+++ src/map/questoes.jsx
@@ -35,3 +35,33 @@ questoes.map((questao) => {
 })
 
 */
+
+function Home(){
+
+	const [questoes, setQuestoes] = useState(
+		[
+			{
+				"texto_questao": "Quanto é 1+1 ?",
+				"img_questao": null
+			},
+			{
+				"texto_questao": "Qual a capital da França ?",
+				"img_questao": null
+			},
+			{
+				"texto_questao": "O que significa a palavra \"Father\" ?",
+				"img_questao": null
+			},
+			{
+				"texto_questao": "Qual a fórmula da relatividade geral de Einstein ?",
+				"img_questao": "eistein.png"
+			},
+			{
+				"texto_questao": "Qual a capital da Alemanha ?",
+				"img_questao": "alemanha.png"
+			}			
+		]
+	);
+
+	return()	
+}
