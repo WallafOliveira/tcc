b553579624258fe1496f807108b8f596fdfd4b39
VictorHerrero16 <herrerovictor05@gmail.com>
2023-10-03 13:47:26 -0300

    03/10
    map

---
+++ src/telaQuestao/questao.jsx
@@ -0,0 +1,48 @@
+/*import React, {useState} from "react";
+
+function Questao() {
+
+    const navigate = useNavigate();
+   
+    const [questoes, setQuestoes] = useState(
+        [
+            {
+                "id_questao": 1,
+                "id_materia": 1,
+                "texto_questao": "Quanto é 1+1 ?",
+                "id_usuario": 22,
+                "img_questao": null
+            },
+            {
+                "id_questao": 2,
+                "id_materia": 4,
+                "texto_questao": "Qual a capital da França ?",
+                "id_usuario": 20,
+                "img_questao": null
+            },
+            {
+                "id_questao": 3,
+                "id_materia": 5,
+                "texto_questao": "O que significa a palavra \"Father\" ?",
+                "id_usuario": 21,
+                "img_questao": null
+            },
+            {
+                "id_questao": 4,
+                "id_materia": 6,
+                "texto_questao": "Qual a fórmula da relatividade geral de Einstein ?",
+                "id_usuario": 22,
+                "img_questao": "eistein.png"
+            },
+            {
+                "id_questao": 5,
+                "id_materia": 4,
+                "texto_questao": "Qual a capital da Alemanha ?",
+                "id_usuario": 22,
+                "img_questao": "alemanha.png"
+            }
+        ]
+   );
+}
+
+export default Questao
