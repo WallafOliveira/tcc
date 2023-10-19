4a27b7b44af4b86cd8c3a3c390ac62f594aa10ba
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-10-19 09:43:31 -0300

    JICARETA

--- src/estatisticas/telaEstatisticas.jsx
+++ src/estatisticas/telaEstatisticas.jsx
@@ -3,11 +3,9 @@ import logo from './logo.png';
 import "./telaEstatisticas.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import Carousel from "./carousel";
-
 function TelaEstatisticas() {
   const navigate = useNavigate();
   const location = useLocation();
-
   return (
     <div>
       <div>
@@ -15,37 +13,33 @@ function TelaEstatisticas() {
           <img className="logo" src={logo} alt='Eduquiz' onClick={() => { navigate('/') }} />
         </nav>
       </div>
-
       <div id="body">
         <div id="carrossel">
           <Carousel /> 
         </div>
-
         <div id="evolucaoDiv">
           <h1 id="textoEvolucao"> Evolução </h1>
-
           <div id="container">
-
             <div id="quizFeitos">
               <h1 id="tituloQuizFeitos"> Total de Quizes Feitos: </h1>
               <p id="numeroQuizes">?</p>
             </div>
-
             <div id="media">
-              <h1 id="tituloMedia"> Média de Acertos </h1>
+              <div id="tituloDiv">
+                <h1 id="tituloMedia"> Média de Acertos </h1>
+              </div>
               <div className="progress-bar">
                 <div className="progress-bar-all">
-                  <div className="progress-bar-fill" />
+                  <div className="progress-bar-fill">
+                  </div>
                 </div>
-                <text className="texto-progress-bar">%</text>
+                <text className="texto-progress-bar">?? %</text>
               </div>
             </div>
-          
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default TelaEstatisticas;
