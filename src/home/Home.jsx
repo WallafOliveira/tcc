e45dbde7afc1e561bf84f6b2b02228a2dee416de
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-10-19 11:26:29 -0300

    .

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -26,28 +26,28 @@ function Home() {
         <h1 id="title">Quer aprender de maneira interativa?</h1>
         <img className="logo2" src={logo2} alt='Eduquiz'/>  
         <div className="sobre">
-        <h1 id="pTitle">Sobre</h1>
-        <br/>
-        <p className="text">
-        Recursos interativos: Tornamos a aprendizagem divertida e envolvente para as crianças. Com elementos visuais atraentes, 
-        animações e feedback imediato sobre as respostas, 
-        incentivamos o interesse e a motivação dos alunos durante o processo de aprendizagem.
-        </p>
-        <br/>
-        <p className="text">
-          Relatórios e avaliações: Receba relatórios detalhados sobre o desempenho de seus filhos em cada questionário realizado. 
-          Com base nesses relatórios, você poderá avaliar o progresso individual, 
-          identificar áreas de força e áreas que necessitam de mais atenção, e tomar medidas educacionais personalizadas para ajudar seus 
-          filhos a alcançarem seu pleno potencial acadêmico.
-        </p>
-        <br/>
-        <p className="text">
-          Controle de Pais:       
-          Acompanhamento do progresso: Obtenha informações detalhadas sobre o desempenho de seus filhos nos questionários. 
-          Nosso software registra as respostas corretas e incorretas,
-          permitindo que você identifique as áreas em que eles podem precisar de mais apoio e orientação. Acompanhe o progresso ao
-          longo do tempo e observe o crescimento das habilidades educacionais de seus filhos.
-        </p>
+          <h1 id="pTitle">Sobre</h1>
+          <br/>
+          <p className="text">
+          Recursos interativos: Tornamos a aprendizagem divertida e envolvente para as crianças. Com elementos visuais atraentes, 
+          animações e feedback imediato sobre as respostas, 
+          incentivamos o interesse e a motivação dos alunos durante o processo de aprendizagem.
+          </p>
+          <br/>
+          <p className="text">
+            Relatórios e avaliações: Receba relatórios detalhados sobre o desempenho de seus filhos em cada questionário realizado. 
+            Com base nesses relatórios, você poderá avaliar o progresso individual, 
+            identificar áreas de força e áreas que necessitam de mais atenção, e tomar medidas educacionais personalizadas para ajudar seus 
+            filhos a alcançarem seu pleno potencial acadêmico.
+          </p>
+          <br/>
+          <p className="text">
+            Controle de Pais:       
+            Acompanhamento do progresso: Obtenha informações detalhadas sobre o desempenho de seus filhos nos questionários. 
+            Nosso software registra as respostas corretas e incorretas,
+            permitindo que você identifique as áreas em que eles podem precisar de mais apoio e orientação. Acompanhe o progresso ao
+            longo do tempo e observe o crescimento das habilidades educacionais de seus filhos.
+          </p>
         </div>
       </div>
     </div>
