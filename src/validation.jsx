a5c8d08f6e8fe6c167287f8de7282a2cf8c37354
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-10-24 13:56:54 -0300

    24/10

--- src/validation.jsx
+++ src/validation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './validation.css'; 
+import './validation.css';
 
 class RealTimeFormValidation extends Component {
   constructor(props) {
@@ -8,8 +8,13 @@ class RealTimeFormValidation extends Component {
       email: '',
       password: '',
       errors: {
-        email: 'Invalid Email',
+        email: '',
         password: 'Password is necessary',
+      },
+      emailRequirements: {
+        hasAtSymbol: false,
+        hasLettersAfterAt: false,
+        validFormat: false,
       },
       passwordRequirements: {
         length: false,
@@ -23,30 +28,48 @@ class RealTimeFormValidation extends Component {
   validateForm = () => {
     const { email, password } = this.state;
     const errors = {
-      email: 'Invalid Email',
+      email: '',
       password: '',
     };
 
     // Email validation
-    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
-      errors.email = 'Invalid Email';
+    const hasAtSymbol = email.includes('@');
+    const hasLettersAfterAt = /^[^@]+@[^@]+$/.test(email);
+    const validFormat = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email);
+
+    if (!email) {
+      errors.email = 'Email is required';
+    } else {
+      if (!hasAtSymbol) {
+        errors.email = 'Email must contain "@"';
+      } else if (!hasLettersAfterAt) {
+        errors.email = 'Email must have letters after "@"';
+      } else if (!validFormat) {
+        errors.email = 'Invalid Email';
+      }
     }
 
-    // Password validation
-    const passwordRequirements = {
-      length: password.length >= 8,
-      number: /\d/.test(password),
-      specialCharacter: /[!@#$%^&*]/.test(password),
-      uppercaseLetter: /[A-Z]/.test(password),
+    const emailRequirements = {
+      hasAtSymbol,
+      hasLettersAfterAt,
+      validFormat,
     };
 
-    if (password) {
-      errors.password = '';
-    } else {
-      errors.password = 'Password is necessary';
-    }
+        // Password validation
+        const passwordRequirements = {
+          length: password.length >= 8,
+          number: /\d/.test(password),
+          specialCharacter: /[!@#$%^&*]/.test(password),
+          uppercaseLetter: /[A-Z]/.test(password),
+        };
+    
+        if (password) {
+          errors.password = '';
+        } else {
+          errors.password = 'Password is necessary';
+        }
 
-    this.setState({ errors, passwordRequirements });
+    this.setState({ errors, emailRequirements, passwordRequirements });
   };
 
   handleInputChange = (e) => {
@@ -59,50 +82,61 @@ class RealTimeFormValidation extends Component {
     this.validateForm();
     const { email, password, errors } = this.state;
 
-    // Check if there are no errors, and submit the form
     if (!errors.email && !errors.password) {
       console.log('Form submitted with email: ', email, ' and password: ', password);
     }
   };
 
   render() {
-    const { email, password, errors, passwordRequirements } = this.state;
+    const { email, password, errors, emailRequirements, passwordRequirements } = this.state;
 
     return (
       <div>
         <h2>Real-Time Form Validation</h2>
-        <form onSubmit={this.handleSubmit}>
+        <form id="test" onSubmit={this.handleSubmit}>
           <div>
             <label>Email:</label>
             <input
-              type="text"
+              className={`entrada ${errors.email ? 'invalid' : emailRequirements.validFormat ? 'valid' : ''}`}
+              type="email"
               name="email"
               value={email}
               onChange={this.handleInputChange}
             />
-            <div className={`error ${errors.email ? 'invalid' : ''}`}>{errors.email}</div>
+            <div className={`error ${errors.email ? 'invalid' : ''}`}/>
+            <ul className="email-requirements">
+              <li className={`email-requirement ${emailRequirements.hasAtSymbol ? 'valid' : 'invalid'}`}>
+                Email must contain "@"
+              </li>
+              <li className={`email-requirement ${emailRequirements.hasLettersAfterAt ? 'valid' : 'invalid'}`}>
+                Email must have letters after "@"
+              </li>
+              <li className={`email-requirement ${emailRequirements.validFormat ? 'valid' : 'invalid'}`}>
+                Email is valid
+              </li>
+            </ul>
           </div>
 
           <div>
             <label>Password:</label>
-            <input
+            <input className={`entrada ${errors.password ? 'invalid' : ''}`}
               type="password"
               name="password"
               value={password}
               onChange={this.handleInputChange}
             />
-            <div className={`error ${errors.password ? 'invalid' : ''}`}>{errors.password}</div>
+            <div className={`error ${errors.password ? 'invalid' : ''}`}></div>
             <ul className="password-requirements">
-              <li className={`password-requirement ${!passwordRequirements.length ? 'invalid' : 'valid'}`}>
+              <li className={`password-requirement ${passwordRequirements.length ? 'valid' : 'invalid'}`}>
                 Must be at least 8 characters long
               </li>
-              <li className={`password-requirement ${!passwordRequirements.number ? 'invalid' : 'valid'}`}>
+              <li className={`password-requirement ${passwordRequirements.number ? 'valid' : 'invalid'}`}>
                 Must contain at least one number
               </li>
-              <li className={`password-requirement ${!passwordRequirements.specialCharacter ? 'invalid' : 'valid'}`}>
+              <li className={`password-requirement ${passwordRequirements.specialCharacter ? 'valid' : 'invalid'}`}>
                 Must contain at least one special character
               </li>
-              <li className={`password-requirement ${!passwordRequirements.uppercaseLetter ? 'invalid' : 'valid'}`}>
+              <li className={`password-requirement ${passwordRequirements.uppercaseLetter ? 'valid' : 'invalid'}`}>
                 Must contain at least one uppercase letter
               </li>
             </ul>
