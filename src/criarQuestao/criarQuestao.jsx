e018b1343251b230b43acb6a7f9520991b6ab5cb
VICTORRFCHIOZZINI <127430122+VICTORRFCHIOZZINI@users.noreply.github.com>
2023-11-28 22:52:31 -0300

    nove

--- src/criarQuestao/criarQuestao.jsx
+++ src/criarQuestao/criarQuestao.jsx
@@ -17,16 +17,20 @@ function CriarQuestao() {
     // Função para atualizar o texto de uma alternativa com base no ID
     const handleInputChange = (id, text) => {
         // Usando a função setAlternativas para atualizar o estado
-        setAlternativas(prevAlternativas => {
-            return prevAlternativas.map(alternativa => {
-                if (alternativa.id === id) {
-                    // Se o ID da alternativa corresponder, atualiza o texto
-                    return {...alternativa, text};
-                }
-                // Mantém as outras alternativas inalteradas
-                return alternativa;
-            });
-        });
+        setAlternativas(
+            prevAlternativas => {
+                return prevAlternativas.map(
+                    alternativa => {
+                        if (alternativa.id === id) {
+                            // Se o ID da alternativa corresponder, atualiza o texto
+                            return {...alternativa, text};
+                        }
+                        // Mantém as outras alternativas inalteradas
+                        return alternativa;
+                    }
+                );
+            }
+        );
     };
     // O componente renderiza o seguinte HTML JSX
     return (
@@ -34,7 +38,7 @@ function CriarQuestao() {
             <div>
                 {/* Cabeçalho com um logotipo clicável que navega para a página inicial */}
                 <nav className="cabeçalho">
-                    <img className="logo" src={logo} alt='Eduquiz' onClick={() => { navigate('/')}}/>
+                    <img className="logo" src={logo} alt='Eduquiz' onClick={() => {navigate('/')}} />
                 </nav>
             </div>
             <div id="enunciadoDiv">
@@ -43,16 +47,18 @@ function CriarQuestao() {
             </div>
             <div id="alternativasDiv">
                 {/* Mapeia as alternativas e renderiza campos de texto para cada uma */}
-                {alternativas.map(alternativa => (
-                    <textarea
-                        key={alternativa.id}
-                        style={{backgroundColor: "#d9d9d9"}}
-                        className="alternativa"
-                        placeholder={`Texto da alternativa ${alternativa.id}`}
-                        value={alternativa.text}
-                        onChange={(e) => handleInputChange(alternativa.id, e.target.value)}
-                    />
-                ))}
+                {alternativas.map(
+                    alternativa => (
+                        <textarea
+                            key={alternativa.id}
+                            style={{backgroundColor: "#d9d9d9"}}
+                            className="alternativa"
+                            placeholder={`Texto da alternativa ${alternativa.id}`}
+                            value={alternativa.text}
+                            onChange={(e) => handleInputChange(alternativa.id, e.target.value)}
+                        />
+                    )
+                )}
             </div>
             <div id="divButtonCriar">
                   <button id="criarButton" onClick={ () => {window.location.reload()}}> Criar </button>
@@ -61,7 +67,4 @@ function CriarQuestao() {
     );
 }
 export default CriarQuestao;
-/* Resumidamente, este código cria um componente React que renderiza um formulário de criação de questões com campos de texto para o enunciado e as alternativas (A, B, C, D).
-O estado é usado para rastrear o texto inserido pelo usuário nas alternativas, e a função handleInputChange é chamada quando o usuário digita em um campo de alternativa para 
-atualizar o estado correspondente. O logotipo no cabeçalho é clicável e redireciona para a página inicial usando o hook de navegação do react-router-dom.
-*/ 
+/* Resumidamente, este código cria um componente React que renderiza um formulário de criação de questões com campos de texto para o enunciado e as alternativas (A, B, C, D). O estado é usado para rastrear o texto inserido pelo usuário nas alternativas, e a função handleInputChange é chamada quando o usuário digita em um campo de alternativa para atualizar o estado correspondente. O logotipo no cabeçalho é clicável e redireciona para a página inicial usando o hook de navegação do react-router-dom. */ 
