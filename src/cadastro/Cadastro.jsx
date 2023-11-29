2f6640a9bf145091ed9a22836c456aca9205bbbd
VICTORRFCHIOZZINI <127430122+VICTORRFCHIOZZINI@users.noreply.github.com>
2023-11-28 22:47:37 -0300

    sete

--- src/cadastro/Cadastro.jsx
+++ src/cadastro/Cadastro.jsx
@@ -10,22 +10,22 @@ function Cadastro() {
             <nav className="cabeçalho">
                 <img className="logo" src={logo} alt='Eduquiz' onClick={() => {navigate('/')}}/> 
             </nav>
-        <div class="container_cadastro">
-            <span id="cadastroTitle">Faça seu Cadastro</span>
-            <div class="email_password">
-                <input class="input" type="text" placeholder="Nome" />
-                <input class="input" type="number" placeholder="Idade" />
-                <input class="input" type="email" placeholder="Email" />
-                <input class="input" type="password" placeholder="Senha" />
+            <div class="container_cadastro">
+                <span id="cadastroTitle">Faça seu Cadastro</span>
+                <div class="email_password">
+                    <input class="input" type="text" placeholder="Nome" />
+                    <input class="input" type="number" placeholder="Idade" />
+                    <input class="input" type="email" placeholder="Email" />
+                    <input class="input" type="password" placeholder="Senha" />
+                </div>
+                <div class="btn_cad">
+                    <button class="btn" onClick={() => {navigate('/homeusuario')}}>Cadastrar</button>
+                </div>
+                <div class="rede_login">
+                    <img class='logo_social' src="https://img.freepik.com/icones-gratis/google_318-278809.jpg" alt="Google" onchange="$"/>
+                    <img class='logo_social' src="https://www.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png" alt="Facebook" onchange="$"/>
+                </div>
             </div>
-            <div class="btn_cad">
-                <button class="btn" onClick={() => {navigate('/homeusuario')}}>Cadastrar</button>
-            </div>
-            <div class="rede_login">
-                <img class='logo_social' src="https://img.freepik.com/icones-gratis/google_318-278809.jpg" alt="Google" onchange="$"/>
-                <img class='logo_social' src="https://www.facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png" alt="Facebook" onchange="$"/>
-            </div>
-        </div>
         </div>
     );
 }
